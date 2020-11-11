0d831b6084385acb0cb2c179473e3eed486ac7f1
Angela Mitchell <anglarenmit@gmail.com>
2020-11-10 20:30:25 -0600

    Added data to astronaut objects

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -11,13 +11,13 @@ export class CrewComponent implements OnInit {
   crew: object[] = [];
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {name: "Sally Ride", active: false, skills: ["Physicist, Astrophysics"], hoursInSpace: 343, photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
+    {name: "Mae Jemison", active: false, skills: ["Physician", "Chemical Engineer"], hoursInSpace: 190, photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
+    {name: "Ellen Ochoa", active: false, skills: ["Physics", "Electrical Engineer"], hoursInSpace: 979, photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
+    {name: "Frederick Gregory", active: false, skills: ["Information Systems", "Shuttle Pilot", "Fighter Pilot", "Helicopter Pilot", "Colonel USAF"], hoursInSpace: 455, photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
+    {name: "Guion Bluford", active: false, skills: ["Aerospace Engineer", "Philosophy", "Physics", "Colonel USAF", "Fighter Pilot"], hoursInSpace: 686, photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
+    {name: "Kjell Lindgren", active: true, skills: ["Physician", "Surgeon", "Emergency Medicine"], hoursInSpace: 15, photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
+    {name: "Jeanette Epps", active: true, skills: ["Physicist", "Philosophy", "Aerospace Engineer"], hoursInSpace: 0, photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
   ];
 
   constructor() { }
